4d87fc377721f278ebf53100699164bcc796d9c6
Danny Murray <murraydp@mail.uc.edu>
2024-06-27 19:59:19 -0400

    Added Enemy State Machine Frameworks

--- Assets/_app/Scripts/Enemy/States/EnemyState.cs
+++ Assets/_app/Scripts/Enemy/States/EnemyState.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 namespace _app.Scripts.Enemy.States {
     public class EnemyState {
-        
+        // ===== Fields =====
+        protected Enemy Enemy;
+        protected EnemyStateMachine StateMachine;
+
+        // ===== Constructors =====
+        public EnemyState(Enemy enemy, EnemyStateMachine stateMachine) {
+            Enemy = enemy;
+            StateMachine = stateMachine;
+        }
+
+        // ===== Methods =====
+        public virtual void EnterState() {}
+
+        public virtual void ExitState() {}
+
+        public virtual void FrameUpdate() {}
+
+        public virtual void PhysicsUpdate() {}
     }
 }
--- Assets/_app/Scripts/Enemy/States/EnemyStateMachine.cs
+++ Assets/_app/Scripts/Enemy/States/EnemyStateMachine.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 namespace _app.Scripts.Enemy.States {
     public class EnemyStateMachine {
-        
+        // ===== Fields =====
+        public EnemyState State;
+
+        // ===== Constructors =====
+        public EnemyStateMachine(EnemyState initialState) {
+            State = initialState;
+            State.EnterState();
+        }
+
+        // ===== Methods =====
+        public void ChangeState(EnemyState newState) {
+            State.ExitState();
+            State = newState;
+            State.EnterState();
+        }
     }
 }
